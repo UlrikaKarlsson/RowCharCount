b7fd285b67cae9354ccbf73e35b8db46f7b0a265
Ulrika Karlsson <ulrika.karlsson@yahoo.se>
2020-11-04 15:02:49 +0100

    added count lines

---
+++ RowCharCount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RowCharCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            string path = @"c:\FileForSwedishLanguage.txt";
+            string[] linesOfFileText = File.ReadAllLines(path);
+            //read file
+            foreach (string s in linesOfFileText)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
